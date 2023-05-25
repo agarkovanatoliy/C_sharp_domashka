ad2df30f69ae1c6f5d3371d66496f6db16c74680
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-26 00:55:46 +0300

    Вывод третьей цыфры из числа

--- Семинар2/задача13/Program.cs
+++ Семинар2/задача13/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+// Console.Write("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// if ( number < 100)
+// {
+//     Console.WriteLine("Третьего числа нет! ");
+// }
+// else
+// {
+//     while ( number > 999)
+//     {
+//         number = number / 10;
+//     }
+//     number = number % 10;
+//     Console.WriteLine("Третьего число: " + number);
+// }
+
+void ThirdDigit(int arg)
+{
+    if ( arg < 100)
+    {
+        Console.WriteLine("Третьего числа нет! ");
+    }
+    else
+    {
+        while ( arg > 999)
+        {
+            arg = arg / 10;
+        }
+        arg = arg % 10;
+        Console.WriteLine("Третьего число: " + arg);
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+ThirdDigit(number);
