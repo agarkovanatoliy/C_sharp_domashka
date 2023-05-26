5eeea7811cfae4e19407369339068c58f93c29f9
AnatoliyAgarkov <tolifander@gmail.com>
2023-05-26 19:29:52 +0300

    Определение по числу дня недели, рабочий или выходной

--- Семинар2/задача15/Program.cs
+++ Семинар2/задача15/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую 
+// день недели, и проверяет, является ли этот день выходным.
+
+
+
+string[] weekday = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+int length = weekday.Length;
+int index = 0;
+Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Неверное число! ");
+}
+else if (number >= 1 & number <= 5)
+{
+    index = number - 1;
+    Console.WriteLine( weekday[index] + ",  рабочий день!");
+}
+else if ( number == 6 || number == 7)
+{
+    index = number - 1;
+    Console.WriteLine( weekday[index] + ",  выходной день!");
+}
