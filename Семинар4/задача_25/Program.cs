6ded6b66ee6712ebd004adba39acd1f0e9dba5b5
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-04 21:14:08 +0300

    Возведение в степень

---
+++ Семинар4/задача_25/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        Console.WriteLine("показатель не должен быть меньше нуля");
+        return false;
+    }
+    return true;
+}
+
+int Exponentiation(int poverBase, int exsponent)
+{
+    int reselt = 1;
+    for (int i = 0; i < exsponent; i++)
+    {
+        reselt *= poverBase;
+    }
+    return reselt;
+} 
+
+int poverBase = Prompt("Введите основание: ");
+int exponent = Prompt("ВВедите покозатель степени:");
+
+if (ValidateExponent(exponent)) Console.WriteLine($"Число {poverBase} в степени {exponent} равно {Exponentiation(poverBase, exponent)}");
