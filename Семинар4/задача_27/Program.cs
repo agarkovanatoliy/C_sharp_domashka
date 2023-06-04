f0f98152617e46cf7de9a95dc31223b7026f2b37
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-04 21:37:18 +0300

    Сумма цифр числа

---
+++ Семинар4/задача_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int Sum (int num)
+{
+    int result = 0;
+    while ( num > 0)
+    {
+        result += num%10;
+        num = num /10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число: ");
+int sum = Sum(number);
+Console.WriteLine($"Сумма цыфр в числе {number} равна: {sum}");
