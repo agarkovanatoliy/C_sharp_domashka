cccbaf38480f301de2ab3e45c1449a511daa7f1a
AnatoliyAgarkov <tolifander@gmail.com>
2023-06-08 11:50:01 +0300

    Количество положительных чисел

---
+++ Семинар6/задача_41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M
+// чисел. Посчитайте, сколько чисел больше 0 ввёл
+// пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+string Prompt(string message)
+{
+    Console.Write(message);
+    string num = Console.ReadLine();
+    return num;
+}
+
+
+void SumNumber()
+{
+    int sum = 0;
+    int temp = 0;
+    while (sum >= 0)
+    {    
+        // Array.Resize(ref res, res.Length + 1); 
+        string number = Prompt("Введите число или нажмите клавишу Q: ");
+        if (number.ToLower() == "q") break;     
+        else temp = int.Parse(number);
+        if (temp > 0) sum++;
+    }
+    Console.WriteLine($"Пользователь ввел {sum} положительных чисела!");
+}
+
+SumNumber();
